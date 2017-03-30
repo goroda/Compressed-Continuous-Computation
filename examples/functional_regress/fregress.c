09d6bc84476b244f57c681b4347a440de068f603
Alex Gorodetsky <goroda@mit.edu>
2017-03-30 09:52:40 -0600

    converted functional regression into a proper command line utility

--- examples/functional_regress/fregress.c
+++ examples/functional_regress/fregress.c
@@ -5,13 +5,45 @@
 #include <assert.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <getopt.h>
 #include <unistd.h>
 
 #include "c3.h"
 
 /* #define dx 2 */
-#define dx 100
+/* #define dx 100 */
+static size_t dx;
 #define dy 16
+
+static char * program_name;
+
+void print_code_usage (FILE *, int) __attribute__ ((noreturn));
+void print_code_usage (FILE * stream, int exit_code)
+{
+    fprintf(stream, "Two examples of functional regression \n\n");
+    fprintf(stream, "Usage: %s options \n", program_name);
+    fprintf(stream,
+            " -h --help      Display this usage information.\n"
+            " -f --function  Which function to evaluate. \n"
+            "                0: a_1x^2 + a_2x^2 (default)\n"
+            "                1: more complicated, check code\n"
+            " -n --number    Number of training samples (default 100).\n"
+            " -p --polyorder Polynomial order for parameters (default 4).\n"
+            " -b --basis     Basis for spatial variable\n"
+            "                0: piecewise linear continuous (default)\n"
+            "               >0: radial basis function kernels\n"
+            " -s --rankspace Rank between spatial and paramter variables (default 4)\n"
+            " -r --rankparam Rank between parameters (default 2)\n"
+            " -v --verbose   Output words (default 0).\n"
+            " \n\n"
+            " Outputs four files\n"
+            " training_funcs.dat  -- training samples"
+            " testing_funcs_n{number}.dat -- evaluations of true model\n"
+            " testing_funcs_ft_n{number}.dat -- evaluations of reg model\n"
+            " testing_funcs_diff_n{number}.dat -- difference b/w models\n"
+        );
+    exit (exit_code);
+}
 
 struct Problem
 {
@@ -60,15 +92,22 @@ static double * generate_inputs(size_t n)
 static double * generate_outputs(prob_t * prob, size_t n, double * inputs)
 {
     double * y = calloc_double(n * dy);
-    for (size_t ii = 0; ii < n; ii++){
-        /* quadratic(prob,inputs + ii*dx,y+ii*dy); */
-        other(prob,inputs + ii*dx,y+ii*dy);
+    if (dx == 2){
+        for (size_t ii = 0; ii < n; ii++){
+            quadratic(prob,inputs + ii*dx,y+ii*dy);
+        }
+    }
+    else {
+        for (size_t ii = 0; ii < n; ii++){
+            other(prob,inputs + ii*dx,y+ii*dy);
+        }
     }
 
     return y;
 }
 
-static size_t create_unified_data(size_t ndata, double * inputs, double * xspace,
+static size_t create_unified_data(size_t ndata, double * inputs,
+                                  double * xspace,
                                   double * outputs, double * x, double * y)
 {
     size_t ondata=0;
@@ -98,15 +137,98 @@ static void save_array_with_x(size_t nrows, size_t ncols, double * x,
     free(temp);
 }
 
-int main()
+int main(int argc, char * argv[])
 {
+    int next_option;
+    const char * const short_options = "hf:n:p:b:s:r:v:";
+    const struct option long_options[] = {
+        { "help"     , 0, NULL, 'h' },
+        { "function" , 1, NULL, 'f' },
+        { "number"   , 1, NULL, 'n' },
+        { "polyorder", 1, NULL, 'p' },
+        { "basis"    , 1, NULL, 'b' },
+        { "rankspace", 1, NULL, 's' },
+        { "rankparam", 1, NULL, 'r' },
+        { "verbose"  , 1, NULL, 'v' },
+        { NULL       ,  0, NULL, 0   }
+    };
 
+    size_t npoly = 4;
+    size_t ndata = 100;
+    int verbose = 0;
+    size_t function = 0;
+    size_t basis = 0;
+    size_t rank_space = 4;
+    size_t rank_param = 2;
+    program_name = argv[0];
+    do {
+        next_option = getopt_long (argc, argv, short_options, long_options, NULL);
+        switch (next_option)
+        {
+            case 'h': 
+                print_code_usage(stdout, 0);
+            case 'f':
+                function = strtoul(optarg,NULL,10);
+                break;
+            case 'n':
+                ndata = strtoul(optarg,NULL,10);
+                break;
+            case 'p':
+                npoly = strtoul(optarg,NULL,10);
+                break;
+            case 'b':
+                basis = strtoul(optarg,NULL,10);
+                break;
+            case 's':
+                rank_space = strtoul(optarg,NULL,10);
+                break;
+            case 'r':
+                rank_param = strtoul(optarg,NULL,10);
+                break;
+            case 'v':
+                verbose = strtoul(optarg,NULL,10);
+                break;
+            case '?': // The user specified an invalid option 
+                print_code_usage (stderr, 1);
+            case -1: // Done with options. 
+                break;
+            default: // Something unexpected
+                abort();
+        }
+
+    } while (next_option != -1);
+
+
+    if (function == 1){
+        dx = 100;
+    }
+    else{
+        dx = 2;
+    }
+    printf("\n");
+    printf("\n");
+    printf("\t Functional regression setup\n");
+    printf("\n");
+    printf("\t Number of parameters:           %zu\n",dx);
+    printf("\t Number of spatial measurements: %d\n",dy);
+    printf("\t Number of data points:          %zu\n",ndata);
+    printf("\t Basis:                          ");
+    if (basis == 0){
+        printf("piecewise continuous linear elements\n");
+    }
+    else{
+        printf("squared exponential kernels\n");
+    }
+    printf("\t Rank between parameters:        %zu\n",rank_param);
+    printf("\t Rank between param and space:   %zu\n",rank_space);
+    printf("\t Parameter polynomial order:     %zu\n",npoly-1);
+    printf("\n\n\n\n");
+    
     prob_t prob;
     prob.ninput = dx;
     prob.noutput = dy;
     prob.x = linspace(0.0,1.0,dy);
 
-    size_t ndata = 200;
     double * inputs = generate_inputs(ndata);
     double * outputs = generate_outputs(&prob,ndata,inputs);
 
@@ -115,53 +237,68 @@ int main()
     double * x = calloc_double((dx+1)*dy * ndata);
     double * y = calloc_double(ndata*dy);
 
-    size_t ntotdata = create_unified_data(ndata,inputs,prob.x,outputs,x,y);
+    
+    /* size_t ntotdata = create_unified_data(ndata,inputs,prob.x,outputs,x,y); */    
+    create_unified_data(ndata,inputs,prob.x,outputs,x,y);    
 
+    
     /* dprint2d_col(dy,ndata,y); */
-    printf("ntotal data = %zu\n",ntotdata);
+    /* printf("ntotal data = %zu\n",ntotdata); */
 
-    size_t npoly = 4;
+
     struct OpeOpts * opts = ope_opts_alloc(LEGENDRE);
     ope_opts_set_nparams(opts,npoly);
-    struct OneApproxOpts * polyopts = one_approx_opts_alloc(POLYNOMIAL,opts);
-    struct MultiApproxOpts * fapp = multi_approx_opts_alloc(dx+1);
+    struct OneApproxOpts * polyopts =
+        one_approx_opts_alloc(POLYNOMIAL,opts);
 
     double width = pow(dy,-0.2)/sqrt(12.0);
     width *= 0.5;
-    printf("width=%G\n",width);
     struct KernelApproxOpts * kopts =
         kernel_approx_opts_gauss(prob.noutput,prob.x,1.0,width);
-    struct OneApproxOpts * ko = one_approx_opts_alloc(KERNEL,kopts);
-    /* struct LinElemExpAopts * lopts = lin_elem_exp_aopts_alloc(prob.noutput,prob.x); */
-    /* struct OneApproxOpts * ko = one_approx_opts_alloc(LINELM,lopts); */
+    struct LinElemExpAopts * lopts =
+        lin_elem_exp_aopts_alloc(prob.noutput,prob.x);
+
+    struct OneApproxOpts * ko = NULL;
+    if (basis == 0){
+        ko = one_approx_opts_alloc(LINELM,lopts);
+    }
+    else{
+        /* printf("width=%G\n",width); */
+        ko = one_approx_opts_alloc(KERNEL,kopts);
+    }
+
+    struct MultiApproxOpts * fapp = multi_approx_opts_alloc(dx+1);    
     for (size_t ii = 0; ii < dx; ii++){
         multi_approx_opts_set_dim(fapp,ii,polyopts);
     }
     multi_approx_opts_set_dim(fapp,dx,ko);
 
+    
     size_t * ranks = calloc_size_t(dx+1+1);
     ranks[0] = 1;
     ranks[dx+1] = 1;
     for (size_t ii = 1; ii <= dx; ii++){
-        ranks[ii] = 2;
+        ranks[ii] = rank_param;
     }
-    ranks[dx] = 4;
+    ranks[dx] = rank_space;
 
-    printf("here!\n");
     struct FTparam * ftp = ft_param_alloc(dx+1,fapp,NULL,ranks);
     ft_param_create_from_lin_ls(ftp,ndata*dy,x,y,1e-6);
     struct RegressOpts * ropts = regress_opts_create(dx+1,AIO,FTLS);
     struct c3Opt * optimizer = c3opt_create(BFGS);
-    c3opt_set_verbose(optimizer,1);
+    if (verbose > 0){
+        c3opt_set_verbose(optimizer,1);
+    }
     c3opt_set_maxiter(optimizer,10000);
     c3opt_set_gtol(optimizer,1e-15);
     c3opt_set_relftol(optimizer,1e-12);
 
-    printf("there!\n");
     struct FunctionTrain * ft_final =
         c3_regression_run(ftp,ropts,optimizer,ndata,x,y);
 
-    size_t ntest = 30;
+
+    
+    size_t ntest = 100;
     double * test_inputs = generate_inputs(ntest);
     double * test_outputs = generate_outputs(&prob,ntest,test_inputs);
 
@@ -179,9 +316,14 @@ int main()
         }
     }
 
+    
     double diff_se = cblas_ddot(dy*ntest,diff,1,diff,1);
-    printf("Difference squared error = %G\n",diff_se);
-
+    double norm_total = cblas_ddot(dy*ntest,test_outputs,1,test_outputs,1);
+    printf("\n\n\n\t===================================\n\n");
+    printf("\tDifference squared error = %G\n",diff_se);
+    printf("\tSquared norm = %G\n",norm_total);
+    printf("\n\n\n\n");
+    
     char ftest[64];
     char ftest_ft[64];
     char ftest_diff[64];
@@ -194,5 +336,23 @@ int main()
     save_array_with_x(dy,ntest,prob.x,diff,ftest_diff);
     /* dprint(dy,test_outputs); */
     /* dprint(dy,ft_output); */
-    
+
+    free(inputs); inputs = NULL;
+    free(outputs); outputs = NULL;
+    free(x); x = NULL;
+    free(y); y = NULL;
+    one_approx_opts_free_deep(&polyopts); polyopts = NULL;
+    one_approx_opts_free_deep(&ko); ko = NULL; 
+    multi_approx_opts_free(fapp); fapp = NULL;
+    free(ranks); ranks = NULL;
+    ft_param_free(ftp); ftp = NULL;
+    regress_opts_free(ropts); ropts = NULL;
+    c3opt_free(optimizer); optimizer = NULL;
+    function_train_free(ft_final); ft_final = NULL;
+    free(test_inputs); test_inputs = NULL;
+    free(test_outputs); test_outputs = NULL;
+    free(ft_output); ft_output = NULL;
+    free(diff); diff = NULL;
+    free(pt); pt = NULL;
+
 }
