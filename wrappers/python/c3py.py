63339452931b5123661b132c7dbee2e7a24e3987
aagorod <aagorod@sandia.gov>
2017-04-18 17:06:19 -0600

    added close() function python function train class and playing around with regression quality

--- wrappers/python/c3py.py
+++ wrappers/python/c3py.py
@@ -7,12 +7,9 @@ class FunctionTrain:
 
     """ 
     Need to clean up memory
-    1) optimizer
-    2) ft
     3) multipts
-    4) opts
 
-    Handling multiopts is not ideal. Need a way to copy multiopts
+    Handling multiopts is not ideal. Need a way to copy multiopts, right now its memory is not freed
     """
     def __init__(self,din,filename=None,multiopts=None):
 
@@ -20,9 +17,11 @@ class FunctionTrain:
 
         self.dim = din
         self.opts = []
+        self.onedopts = []
         self.ranks = [1]
         for ii in range(din):
             self.opts.append(None)
+            self.onedopts.append(None)
             self.ranks.append(1)
             
         self.ranks.insert(din,1)
@@ -41,16 +40,33 @@ class FunctionTrain:
             raise AttributeError('cannot call set_dim_opts because was already called')
 
         if ftype == "legendre":
-            self.opts.insert(dim,("poly",c3.ope_opts_alloc(c3.LEGENDRE)))
+            self.opts.insert(dim,["poly",c3.ope_opts_alloc(c3.LEGENDRE)])
             c3.ope_opts_set_lb(self.opts[dim][1],lb)
             c3.ope_opts_set_ub(self.opts[dim][1],ub)
             c3.ope_opts_set_nparams(self.opts[dim][1],nparam)
         elif ftype == "hermite":
-            self.opts.insert(dim,("poly",c3.ope_opts_alloc(c3.HERMITE)))
+            self.opts.insert(dim,["poly",c3.ope_opts_alloc(c3.HERMITE)])
+            c3.ope_opts_set_nparams(self.opts[dim][1],nparam)
         else:
             raise AttributeError('No options can be specified for function type ' + ftype)
             
 
+    def _build_multiopts(self):
+        for ii in range(self.dim):
+            if self.onedopts is not None:
+                c3.one_approx_opts_free(self.onedopts[ii])
+            
+            if self.opts[ii][0] == "poly":
+                self.onedopts.insert(ii,c3.one_approx_opts_alloc(c3.POLYNOMIAL,self.opts[ii][1]))
+            elif self.opts[ii][0] == "linelm":
+                self.onedopts.insert(ii,c3.one_approx_opts_alloc(c3.LINELM,self.opts[ii][1]))
+            elif self.opts[ii][0] == "kernel":
+                self.onedopts.insert(ii,c3.one_approx_opts_alloc(c3.KERNEL,self.opts[ii][1]))
+            else:
+                raise AttributeError("Don't know what to do here")
+
+            c3.multi_approx_opts_set_dim(self.multiopts,ii,self.onedopts[ii])
+     
     def set_ranks(self,ranks):
 
         if (isinstance(ranks,list)):
@@ -71,19 +87,8 @@ class FunctionTrain:
 
         if verbose > 1:
             c3.c3opt_set_verbose(self.optimizer,1)
-        
-        self.qmopts = []
-        for ii in range(self.dim):
-            if self.opts[ii][0] == "poly":
-                self.qmopts.append(c3.one_approx_opts_alloc(c3.POLYNOMIAL,self.opts[ii][1]))
-            elif self.opts[ii][0] == "linelm":
-                self.qmopts.append(c3.one_approx_opts_alloc(c3.LINELM,self.opts[ii][1]))
-            elif self.opts[ii][0] == "kernel":
-                self.qmopts.append(c3.one_approx_opts_alloc(c3.KERNEL,self.opts[ii][1]))
-            else:
-                raise AttributeError("Don't know what to do here")
 
-            c3.multi_approx_opts_set_dim(self.multiopts,ii,self.qmopts[ii])
+        self._build_multiopts()
                 
         reg = c3.ft_regress_alloc(self.dim,self.multiopts,self.ranks)
         if alg == "AIO" and obj == "LS":
@@ -116,13 +121,13 @@ class FunctionTrain:
     def __add__(self,other,eps=1e-14):
         out = FunctionTrain(self.dim,multiopts=self.multiopts)
         out.ft = c3.function_train_sum(self.ft,other.ft)
-        out.round()
+        out.round(eps)
         return out
         
     def __mul__(self,other,eps=1e-14):
         out = FunctionTrain(self.dim,multiopts=self.multiopts)
         out.ft = c3.function_train_product(self.ft,other.ft)
-        out.round()
+        out.round(eps)
         return out
 
     def integrate(self):
@@ -136,3 +141,39 @@ class FunctionTrain:
 
     def expectation(self):
         return c3.function_train_integrate_weighted(self.ft)
+
+    def close(self):
+
+        if self.optimizer is not None:
+            c3.c3opt_free(self.optimizer)
+            self.optimizer = None
+
+        if self.ft is not None:
+            c3.function_train_free(self.ft)
+            self.ft = None
+
+            
+        for ii in range(self.dim):
+            if self.opts[ii] is not None:
+                if (self.opts[ii][0] == "poly"):
+                    c3.ope_opts_free(self.opts[ii][1])
+                elif (self.opts[ii][0] == "kernel"):
+                    c3.kernel_approx_opts_free(self.opts[ii][1])
+                elif (self.opts[ii][0] == "linelm"):
+                    c3.lin_elem_exp_aopts_free(self.opts[ii][1])
+                elif (self.opts[ii][0] == "piecewise"):
+                    c3.piecewise_poly_opts_free(self.opts[ii][1])
+
+                self.opts[ii] = None
+
+            if self.onedopts[ii] is not None:
+                c3.one_approx_opts_free(self.onedopts[ii])
+                self.onedopts[ii] = None
+
+        # print ("free multiopts")
+        # if self.multiopts is not None:
+        #     c3.multi_approx_opts_free(self.multiopts)
+        #     self.multiopts = None
+
+        # print ("done closing")
+        
--- wrappers/python/pytest.py
+++ wrappers/python/pytest.py
@@ -6,38 +6,41 @@ def func1(x):
     return np.sum(x,axis=1)
 
 def func2(x):
-    return np.prod(x+1,axis=1)
+    # return np.prod(x-1,axis=1)
+    return np.log10(np.sum(x,axis=1)**2)
 
-dim = 20
-ndata = 200
-x = np.random.rand(ndata,dim)*2.0-1.0
+dim = 25
+ndata = 500
+# x = np.random.rand(ndata,dim)*2.0-1.0
+x = np.random.randn(ndata,dim)
 y1 = func1(x)
 y2 = func2(x)
 
 lb = -1
 ub = 1
-nparam = 2
-
+nparam = 4
 
 ft = c3py.FunctionTrain(dim)
 for ii in range(dim):
     ft.set_dim_opts(ii,"legendre",lb,ub,nparam)
+    # ft.set_dim_opts(ii,"hermite",nparam)
 
 # ft.set_ranks(ranks)
-ft.build_data_model(ndata,x,y1,alg="AIO",obj="LS",adaptrank=1,verbose=0)
+ft.build_data_model(ndata,x,y1,alg="AIO",obj="LS",adaptrank=1,kickrank=1,roundtol=1e-10,verbose=0)
 
 ft2 = c3py.FunctionTrain(dim)
 for ii in range(dim):
     ft2.set_dim_opts(ii,"legendre",lb,ub,nparam)
+    # ft2.set_dim_opts(ii,"hermite",nparam)
 
-ft2.build_data_model(ndata,x,y2,alg="AIO",obj="LS",adaptrank=1,verbose=0)
+ft2.build_data_model(ndata,x,y2,alg="AIO",obj="LS",adaptrank=1,kickrank=1,roundtol=1e-10,verbose=1)
 
 ft3 = ft + ft2
 ft4 = ft * ft2
 
-test_pt = np.random.randn(dim)
-ft1eval = ft.eval(test_pt)
+test_pt = np.random.rand(dim)*2.0-1.0
 ft2eval = ft2.eval(test_pt)
+ft1eval = ft.eval(test_pt)
 ft3eval = ft3.eval(test_pt)
 ft4eval = ft4.eval(test_pt)
 eval1s = func1(test_pt.reshape((1,dim)))
@@ -50,6 +53,11 @@ print("Fteval =",ft2eval, "Should be =",eval2s)
 print("Fteval =",ft3eval, "Should be =",eval3s)
 print("Fteval =",ft4eval, "Should be =",eval4s)
 
+
+ft.close()
+ft2.close()
+ft3.close()
+ft4.close()
 # mr = c3.function_train_get_maxrank(ft)
 
 
