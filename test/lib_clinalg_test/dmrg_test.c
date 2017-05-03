dcb3b24679bf86868b85665556ee5e448ec74623
Alex Gorodetsky <goroda@mit.edu>
2017-05-02 20:58:33 -0600

    fixed some compiler warnings in the python code and lowered threshold in dmrg unit test

--- test/lib_clinalg_test/dmrg_test.c
+++ test/lib_clinalg_test/dmrg_test.c
@@ -565,7 +565,7 @@ void Test_diffusion_op_struct(CuTest * tc)
     struct FunctionTrain * shouldbe = function_train_copy(ftsum);
     
     double diff = function_train_norm2diff(is,shouldbe);
-    CuAssertDblEquals(tc,0.0,diff*diff,1e-8);
+    CuAssertDblEquals(tc,0.0,diff*diff,1e-7);
 
     for (ii = 0; ii  < dim; ii++){
         qmarray_free(da[ii]);
