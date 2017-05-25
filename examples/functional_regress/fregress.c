9e08de6488138c2159b02670cc395465207ca5a5
aagorod <aagorod@sandia.gov>
2017-05-25 11:40:31 -0600

    fixed compiler warning

--- examples/functional_regress/fregress.c
+++ examples/functional_regress/fregress.c
@@ -105,25 +105,29 @@ static void third(prob_t * prob, double * input, double * output)
 
 static void fourth(prob_t * prob, double * input, double * output)
 {
+    (void)prob;
+    (void)input;
+    (void)output;
+
     /* double mean[19] = {0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, */
     /*                    0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95}; */
 
-    double mean[6] = {0.15, 0.25, 0.45, 0.65, 0.75, 0.85};
-    double stdev[6] = {0.2, 0.2, 0.2, 0.2, 0.2, 0.2};
+    /* double mean[6] = {0.15, 0.25, 0.45, 0.65, 0.75, 0.85}; */
+    /* double stdev[6] = {0.2, 0.2, 0.2, 0.2, 0.2, 0.2}; */
 
-    for (size_t ii = 0; ii < prob->noutput; ii++){
-        output[ii] = 0.0;
-        double val = 0.0;
-        for (size_t jj = 0; jj < prob->ninput; jj++){
-            val += (input[jj] - lb)/(ub-lb);
-        }
+    /* for (size_t ii = 0; ii < prob->noutput; ii++){ */
+    /*     output[ii] = 0.0; */
+    /*     double val = 0.0; */
+    /*     for (size_t jj = 0; jj < prob->ninput; jj++){ */
+    /*         val += (input[jj] - lb)/(ub-lb); */
+    /*     } */
         /* output[ii] += sin(val + prob->x[ii]);AAAAAAAAAAAAAAAA */
         /* for (size_t jj = 0; jj < 6; jj++){ */
         /*     double exponent = -(prob->x[ii]-mean[jj])*(prob->x[ii]-mean[jj]) / (2.0 * stdev[jj] * stdev[jj]); */
         /*     output[ii] += exp(exponent); */
         /* } */
 
-    }
+    /* } */
 }
 
 
