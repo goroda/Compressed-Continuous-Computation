da1dfb12af870571f698ca448bdfd408ef88e967
Alex Gorodetsky <alex@alexgorodetsky.com>
2023-06-20 14:07:20 -0400

    bug when order=5

--- c3_cex/examples/wavelets1d/main.c
+++ c3_cex/examples/wavelets1d/main.c
@@ -23,46 +23,52 @@ void print_code_usage (FILE * stream, int exit_code)
     exit (exit_code);
 }
 
-int eval_qtilde(size_t N, size_t order,
-				 const double * xinput_array,
-				 const double * alpha, double * out)
+int eval_q(size_t N, size_t order,
+		   const double * xinput_array,
+		   const double * alpha, double * out)
 {
 	size_t n = order+1;
 	/* is it faster to do two loops, one with an if statement and one without? */
 	for (size_t sample_ind = 0; sample_ind < N; sample_ind++){
 		double x = xinput_array[sample_ind];
 		/* printf("ii = %zu\n", sample_ind); */
-		if (x >= 0.5) {
-			// loop over order
-			double xin = 1.0;
-			for (size_t jj = 0; jj < n; jj++) {
+		/* if (x >= 0.5) { */
+		// loop over order
+		double xin = 1.0;
+		for (size_t jj = 0; jj < n; jj++) {
 
-				/* \tilde{q} */
+			/* \tilde{q} */
+			/* if (x >= 0.5) { */
+			if (x >= 0.0) {
 				out[sample_ind * n + jj] = xin;
-
-				double xout = 1.0;
-				for (size_t ii = 0; ii < n; ii++) {
-					out[sample_ind * n + jj] += alpha[jj * n + ii]  * xout;
-					xout = xout * x;
-				}
-				xin = xin * x;
 			}
-		}
-		else {
-			double xin = 1.0;
-			for (size_t jj = 0; jj < n; jj++) {
-
-				/* \tilde{q} */
+			else{
 				out[sample_ind * n + jj] = -xin;
-
-				double xout = 1.0;
-				for (size_t ii = 0; ii < n; ii++) {
-					out[sample_ind * n + jj] += alpha[jj * n + ii]  * xout;
-					xout = xout * x;
-				}
-				xin = xin * x;
 			}
+
+			double xout = 1.0;
+			for (size_t ii = 0; ii < n; ii++) {
+				out[sample_ind * n + jj] += alpha[jj * n + ii]  * xout;
+				xout = xout * x;
+			}
+			xin = xin * x;
 		}
+		/* } */
+		/* else { */
+		/* 	double xin = 1.0; */
+		/* 	for (size_t jj = 0; jj < n; jj++) { */
+
+		/* 		/\* \tilde{q} *\/ */
+		/* 		out[sample_ind * n + jj] = -xin; */
+
+		/* 		double xout = 1.0; */
+		/* 		for (size_t ii = 0; ii < n; ii++) { */
+		/* 			out[sample_ind * n + jj] += alpha[jj * n + ii]  * xout; */
+		/* 			xout = xout * x; */
+		/* 		} */
+		/* 		xin = xin * x; */
+		/* 	} */
+		/* } */
 	}
 
 	return 0;
@@ -76,15 +82,101 @@ int eval_r(size_t N, size_t order,
 	/* is it faster to do two loops, one with an if statement and one without? */
 	for (size_t sample_ind = 0; sample_ind < N; sample_ind++){
 		for (int jj = order; jj >= 0; jj--) {
+			/* qvals[sample_ind * n + jj] /= beta[order * n + jj];  */
 			for (size_t kk = jj+1; kk <= order; kk++) {
 				qvals[sample_ind * n + jj] += beta[jj * n + kk] * qvals[sample_ind * n + kk];
 			}
+
+		}
+		/* /\* normalize *\/ */
+		for (int jj = order; jj >= 0; jj--) {
+			qvals[sample_ind * n + jj] /= beta[order * n + jj];
 		}
 	}
 
 	return 0;
 }
 
+void test_q_orthogonality(double lb, double ub, size_t N, size_t order, double * coeff, double *Q)
+{
+	
+	double *x = linspace(lb, ub, N);
+	double *evals = malloc(N * (order+1) * sizeof(double));
+	eval_q(N, order, x, coeff, evals);
+
+	double * orth_mat = calloc((order+1) * (order+1), sizeof(double));
+	for (size_t ii = 0; ii < N-1; ii++){
+		for (size_t jj = 0; jj <= order; jj++) {
+			for (size_t kk = 0; kk <= order;  kk++) {
+				orth_mat[jj * (order+1) + kk] +=
+					0.5 * (evals[ii * (order+1) + jj] * evals[ii * (order+1) + kk] +
+						   evals[(ii+1) * (order+1) + jj] * evals[(ii + 1) * (order+1) + kk]) *
+					(x[ii+1] - x[ii]);
+			}
+		}
+	}
+
+	printf("Numerical Q\n");
+	for (size_t ii = 0; ii <= order; ii++) {
+		for (size_t jj = 0; jj <= order; jj++) {
+			printf("%3.2E ", orth_mat[jj * (order+1) + ii]);
+		}
+		printf("\n");
+	}
+
+	printf("Analytical Q\n");	
+	for (size_t ii = 0; ii <= order; ii++) {
+		for (size_t jj = 0; jj <= order; jj++) {
+			printf("%3.2E ", Q[jj * (order+1) + ii]);
+		}
+		printf("\n");
+	}	
+
+	printf("\n\n\n");
+	free(x); x = NULL;
+	free(evals); evals = NULL;
+}
+
+void test_r_orthogonality(double lb, double ub, size_t N, size_t order, double * coeffA, double * coeffB, double *Q)
+{
+	
+	double *x = linspace(lb, ub, N);
+	double *evals = malloc(N * (order+1) * sizeof(double));
+	eval_q(N, order, x, coeffA, evals);
+	eval_r(N, order, coeffB, evals);
+
+	double * orth_mat = calloc((order+1) * (order+1), sizeof(double));
+	for (size_t ii = 0; ii < N-1; ii++){
+		for (size_t jj = 0; jj <= order; jj++) {
+			for (size_t kk = 0; kk <= order;  kk++) {
+				orth_mat[jj * (order+1) + kk] +=
+					0.5 * (evals[ii * (order+1) + jj] * evals[ii * (order+1) + kk] +
+						   evals[(ii+1) * (order+1) + jj] * evals[(ii + 1) * (order+1) + kk]) *
+					(x[ii+1] - x[ii]);
+			}
+		}
+	}
+
+	printf("Numerical R\n");
+	for (size_t ii = 0; ii <= order; ii++) {
+		for (size_t jj = 0; jj <= order; jj++) {
+			printf("%3.2E ", orth_mat[jj * (order+1) + ii]);
+		}
+		printf("\n");
+	}
+
+	/* printf("Analytical Q\n");	 */
+	/* for (size_t ii = 0; ii <= order; ii++) { */
+	/* 	for (size_t jj = 0; jj <= order; jj++) { */
+	/* 		printf("%3.2E ", Q[jj * (order+1) + ii]); */
+	/* 	} */
+	/* 	printf("\n"); */
+	/* }	 */
+
+	printf("\n\n\n");
+	free(x); x = NULL;
+	free(evals); evals = NULL;
+}
 
 
 /* returns lower triangular beta */
@@ -92,35 +184,10 @@ double * orthogonalize(size_t N, double *A, const double *X, const double *B)
 {
 
 	size_t NN = N+1;
-
-	/* double * AX = calloc(NN * NN,  sizeof(double)); */
-	/* cblas_dsymm(CblasColMajor, CblasLeft, CblasLower, NN, NN, 1.0, A, NN, X, NN, 0.0, AX, NN); */
-
-	/* printf("AX = \n"); */
-	/* for (size_t ii = 0; ii < NN; ii++) { */
-	/* 	for (size_t jj = 0; jj < NN; jj++) { */
-	/* 		printf("%3.5f ", AX[jj * NN + ii]);				 */
-	/* 	} */
-	/* 	printf("\n"); */
-	/* } */
 	
-	
-	/* Q = 3.0X^TB + A*/
+	/* Q = -1.0X^TB + A*/
 	cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, NN, NN, NN,
-				3.0, X, NN, B, NN, 1.0, A, NN);
-
-	printf("Q = \n");
-	for (size_t ii = 0; ii < NN; ii++) {
-		for (size_t jj = 0; jj < NN; jj++) {
-			printf("%3.5f ", A[jj * NN + ii]);				
-		}
-		printf("\n");
-	}	
-		
-	/* /\* Compute Q = A + X^TAX + X^TB + B^TX*\/ */
-	/* cblas_dsyr2k(CblasColMajor, CblasLower, CblasTrans, */
-	/* 			 NN, NN, 1.0, X, NN, B, NN, 1.0, A, NN); */
-
+				-1.0, X, NN, B, NN, 1.0, A, NN);
 
 	double *Q = A;
 	printf("Q = \n");
@@ -131,18 +198,13 @@ double * orthogonalize(size_t N, double *A, const double *X, const double *B)
 		printf("\n");
 	}
 	
-   
 	
-	printf("Lets start iteration\n");
 	double *a = malloc(NN * sizeof(double));
-	/* double a[6]; */
-	printf("whats wrong?");
 	a[N] = Q[N * NN + N];
-	/* a[N] = 1.0; //A[N * NN + N]; */
 
 	printf("a[N] = %3.4f\n", a[N]);
 	/* Transpose might be faster */
-	double *beta = calloc(NN * NN, sizeof(double));
+	double *beta = calloc(NN * NN, sizeof(double));  /* lower triangular, last column has normalizing factors */
 	for (int jj = N-1; jj >= 0; jj-- ) {
 		printf("jj = %d\n", jj);
 		beta[jj * NN + N] = - Q[N * NN + jj] / a[N];		
@@ -154,23 +216,17 @@ double * orthogonalize(size_t N, double *A, const double *X, const double *B)
 		printf("ii = %d\n", ii);
 		/* Compute Un-normalized \beta_{ij} */
 		for (int jj = ii-1; jj >= 0; jj--) {
-			beta[jj * NN + ii] = - Q[ii * N + jj];
+			beta[jj * NN + ii] = - Q[ii * NN + jj];
 			for (size_t kk = ii + 1; kk <= N; kk++) {
-				beta[jj * NN + ii] += beta[ii * NN + kk] * beta[jj * NN + kk] * a[kk];
+				beta[jj * NN + ii] += (beta[ii * NN + kk] * beta[jj * NN + kk] * a[kk]);
 			}
+			printf("beta[%d, %d] = %3.5f\n", ii, jj, beta[jj * NN + ii]);
 		}
 
 		/* Compute <r_i, r_i> */
-		a[ii] = Q[ii * NN + ii] + 2 * beta[ii * NN + N] * Q[N * NN + ii] +
-			a[N] * pow(beta[ii * NN + N], 2);
-
-		qk[N] = Q[N * NN + ii];
-		for (int kk = N - 1; kk >= ii+1; kk--) {
-			qk[kk] = Q[kk * NN + ii] + beta[kk * NN + N] * Q[N * NN + ii];
-			for (int ll = N-1; ll >= kk + 1; ll--) {
-				qk[kk] += beta[ll + kk * NN] * qk[ll];
-			}
-			a[ii] += 2 * beta[kk + ii * NN] * qk[kk] + a[kk] * pow(beta[ii * NN + kk], 2);
+		a[ii] = Q[ii * NN + ii];
+		for (int kk = N ; kk >= ii+1; kk--) {			
+			a[ii] -=  a[kk] * pow(beta[ii * NN + kk], 2);
 		}
 
 		/* Update beta_{ij} */
@@ -178,6 +234,13 @@ double * orthogonalize(size_t N, double *A, const double *X, const double *B)
 			beta[jj * N + ii] /= a[ii];
 		}
 	}
+
+	/* Store normalizing info */
+	for (int ii = N-1; ii >= 0; ii--) {
+		/* printf("ii = %d\n", ii); */
+		beta[N * NN + ii] = sqrt(a[ii]);
+	}
+	beta[N * NN + N] = sqrt(Q[N * NN + N]);
 
 	printf("<r_i, r_i> = \n");
 	for (size_t ii = 0; ii < NN; ii++) {
@@ -219,7 +282,7 @@ int main(int argc, char * argv[])
     } while (next_option != -1);
 
 
-	size_t order = 4;
+	size_t order = 5;
 	size_t n = order + 1;
 	size_t n2 = n * n;
 
@@ -227,10 +290,18 @@ int main(int argc, char * argv[])
 	double *B = malloc(n2 * sizeof(double));
 	for (size_t ii = 0; ii <= order; ii++){
 		for (size_t jj = 0; jj <= order; jj++) {
-			A[ii * n + jj] = 1.0 / ((double) ii + jj + 1.0);
-			B[ii * n + jj] = (1 - pow(0.5, ii + jj)) * A[ii * n + jj];
+			/* A[ii * n + jj] = 1.0 / ((double) ii + jj + 1.0); */
+			/* B[ii * n + jj] = -(1 - pow(0.5, ii + jj)) * A[ii * n + jj]; */
+			
+			A[ii * n + jj] = 1.0 / ((double) ii + jj + 1.0) - (pow(-1.0,ii+jj+1) / ((double) ii + jj + 1.0)) ;
+			B[ii * n + jj] = -(1.0 / ((double) ii + jj + 1.0) + (pow(-1.0,ii+jj+1) / ((double) ii + jj + 1.0)));
 		}
 	}
+
+	/* Verified */
+	printf("A = \n");
+	dprint2d_col(n, n, A);
+	
 	double *X = malloc(n2 * sizeof(double));
 	memmove(X, B, n2 * sizeof(double));
 
@@ -242,7 +313,7 @@ int main(int argc, char * argv[])
 	int lwork = -1;
 	int info = 0;
 	printf("B = \n");
-	dprint2d(n, n, B);
+	dprint2d_col(n, n, B);
 	dsysv_("L", (int*)&n, (int*)&n, A, (int*)&n, ipiv, X, (int*)&n, &work_opt, &lwork, &info);
 	printf("optimal work = %zu\n", (size_t)work_opt);
 	printf("info = %d\n", info);
@@ -255,42 +326,108 @@ int main(int argc, char * argv[])
 	assert( info == 0 );
 	
 	printf("X = \n");
-	dprint2d(n, n, X);
+	dprint2d_col(n, n, X);
 
-	double lb = 0.0;	
-	double ub = 1.0;
-	size_t N = 1000;
+
+	double * Bcheck = malloc(n * n * sizeof(double));
+	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1.0, Acopy, n, X, n, 0.0, Bcheck, n);
+
+	printf("Bcheck = \n");
+	dprint2d_col(n, n, Bcheck);
+	/* exit(1); */
+
+	double * beta = orthogonalize(order, Acopy, X, B);
+
+	test_q_orthogonality(-1.0, 1.0, 1000, order, X, Acopy);
+	test_r_orthogonality(-1.0, 1.0, 1000000, order, X, beta,  Acopy);
+	
+	/* Acopy becomes Q */
+	/* check orthogonality */
+	/* for (size_t ii = 0; ii < n; ii++){ */
+	/* 	for (size_t jj = 0; jj < ii; jj++) { */
+	/* 		printf("Checking orthogonality between r_{%zu} and r_{%zu}\n", ii, jj); */
+	/* 		double inner = -beta[jj * n + ii] * pow(beta[(order)*n + ii], 2); */
+	/* 		for (size_t kk = ii+1; kk < n; kk++) { */
+	/* 			inner -= beta[ii * n + kk] * beta[jj * n + kk] * pow(beta[(order) * n + kk], 2); */
+	/* 		} */
+	/* 		inner /= beta[(order) * n + ii]; */
+	/* 		printf("\t inner = %3.5E\n", inner); */
+	/* 	} */
+	/* } */
+	
+	printf("Final Q = %3.5E \n", sqrt(Acopy[order * n + order]));
+	exit(1);
+
+	
+	printf("beta = \n");
+	for (size_t ii = 0; ii < n; ii++){
+		for (size_t jj = 0; jj < n; jj++){
+			printf("%3.2E ", beta[ii + jj * n]);
+		}
+		printf("\n");
+	}
+	
+	double lb = -1.0;	
+	double ub = -1e-15;
+	size_t N = 100000;
+	/* size_t N = 1000; */
 	double * x = linspace(lb, ub, N);	
 	double * qvals = malloc(N * n * sizeof(double));
-	int res = eval_qtilde(N, order, x, X, qvals);
+	int res = eval_q(N, order, x, X, qvals);
 	assert (res == 0);
 
 	FILE * fp = fopen("psi_non_orth.dat", "w");
 	assert (fp != NULL);
 	for (size_t ii = 0; ii < N; ii++){
 		for (size_t jj = 0; jj < n; jj++){
-			fprintf(fp, "%3.5f ", qvals[ii * n +jj]);
+			fprintf(fp, "%3.15f ", qvals[ii * n + jj]);
 		}
 		fprintf(fp, "\n");
 	}
 	fclose(fp);
 
-	/* TODO: Add test for expression for <q_j, q_i> as everything depends on it Q matches from python */
-	/* but r is not orthogonal! */
-	
-	double * beta = orthogonalize(order, Acopy, X, B);
-	
 	int res2 = eval_r(N, order, beta, qvals); /* qvals becomes rvals */
 	assert (res2 == 0);
 	fp = fopen("psi_orth.dat", "w");
 	assert (fp != NULL);
 	for (size_t ii = 0; ii < N; ii++){
 		for (size_t jj = 0; jj < n; jj++){
-			fprintf(fp, "%3.5f ", qvals[ii * n +jj]);
+			fprintf(fp, "%3.15f ", qvals[ii * n +jj]);
 		}
 		fprintf(fp, "\n");
 	}
 	fclose(fp);
+	free(x);
+	free(qvals);
+
+	lb = 1e-15;	
+	ub = 1.0;
+	x = linspace(lb, ub, N);	
+	qvals = malloc(N * n * sizeof(double));
+	res = eval_q(N, order, x, X, qvals);
+	assert (res == 0);
+
+	fp = fopen("psi_non_orth2.dat", "w");
+	assert (fp != NULL);
+	for (size_t ii = 0; ii < N; ii++){
+		for (size_t jj = 0; jj < n; jj++){
+			fprintf(fp, "%3.15f ", qvals[ii * n + jj]);
+		}
+		fprintf(fp, "\n");
+	}
+	fclose(fp);
+
+	res2 = eval_r(N, order, beta, qvals); /* qvals becomes rvals */
+	assert (res2 == 0);
+	fp = fopen("psi_orth2.dat", "w");
+	assert (fp != NULL);
+	for (size_t ii = 0; ii < N; ii++){
+		for (size_t jj = 0; jj < n; jj++){
+			fprintf(fp, "%3.15f ", qvals[ii * n +jj]);
+		}
+		fprintf(fp, "\n");
+	}
+	fclose(fp);	
 
 	
 	free(qvals); qvals = NULL;
--- c3_cex/examples/wavelets1d/plot.py
+++ c3_cex/examples/wavelets1d/plot.py
@@ -1,31 +1,96 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
+np.set_printoptions(precision=4)
+def inner_mat(data, data2, x):
 
+    # trapezoid rule
+    out = np.zeros((data.shape[1], data2.shape[1]))
+    for ii in range(data.shape[1]):
+        for jj in range(data2.shape[1]):
+            dd = data[:, ii] * data2[:, jj]
+            out[ii, jj] = np.sum(0.5 * (dd[1:] + dd[:-1]) * (x[1:] - x[:-1]))
+    
+    return out;
+
+    
 if __name__ == "__main__":
 
     filename1 = "psi_non_orth.dat"
     data = np.loadtxt(filename1)
     N = data.shape[0]
-    x = np.linspace(0, 1, N)
+    x = np.linspace(-1, 1, N)
+    # x = np.linspace(0, 1, N)
+    p = np.zeros((N, data.shape[1]))
+    for ii in range(data.shape[1]):
+        p[:, ii] = x**ii
 
-    plt.figure()
-    plt.plot(x, data)
-    plt.title("Non orthogonal")
+    qtilde = np.copy(p)
+    for ii in range(data.shape[1]):
+        qtilde[x<0.0, ii] = -qtilde[x<0.0, ii]
+    
+    A = inner_mat(p, p, x)
+    B = inner_mat(qtilde, p, x)
+    # B = np.dot(qtilde.T, p).T / N
+    # X = np.linalg.solve(A, -B)
+    print("A: \n", A)
+    print("B: \n", B)
+    # print("X: \n", X)
+    # data_should = qtilde + np.dot(X.T, p.T).T
 
-    Q = np.dot(data.T, data) / N
+    # plt.figure()
+    # plt.plot(x, data)
+    # # plt.plot(x, data_should)
+    # # plt.plot(x, p)
+    # plt.title("Non orthogonal")
+    
+    # Q = np.dot(data.T, data) / N
+    Q = inner_mat(data, data, x)
+    # Qshould = np.dot(data_should.T, data_should) / N
     print("Q = \n", Q)
+    # print("Q_should = \n", Qshould)
 
+    qtp = inner_mat(data, p, x)
+    print("q^Tp: \n", qtp )
     
-    filename2 = "psi_orth.dat"
-    data = np.loadtxt(filename2)
-    N = data.shape[0]
-    x = np.linspace(0, 1, N)
+    # print("q^Tr: \n", np.dot(data_should.T, p) / N )
+    # plt.show()
+    # exit(1)
+    # QTR =
+    
+    # beta11 = -Q[-2, -1] / Q[-1, -1]
+    # print("beta11 = ", beta11)
+    
+    filename2a = "psi_orth.dat"
+    filename2b = "psi_orth2.dat"
+    
+    datar1 = np.loadtxt(filename2a)
+    datar2 = np.loadtxt(filename2b)
+    N = datar1.shape[0]
+    x1 = np.linspace(-1, -1e-15, N)
+    x2 = np.linspace(1e-15, 1, N)
 
-    R = np.dot(data.T, data) / N
+    R1 = inner_mat(datar1, datar1, x1)
+    R2 = inner_mat(datar2, datar2, x2)
+    R = R1 + R2
     print("R = \n", R)
+
+    # rtp = inner_mat(datar, p, x)
+    # # print("q^Tp: \n", np.dot(data.T, p) / N )
+    # print("r^tp = \n", rtp)
+    # print("q^tp = \n", np.dot(data.T, p) / N)
     
-    plt.figure()
-    plt.plot(x, data)
-    plt.title("Should be orthogonal")
-    plt.show()
+    # datar_should = np.zeros((N, datar.shape[1]))
+    # datar_should[:, -1] = data_should[:, -1]
+    # datar_should[:, -2] = data_should[:, -2] + beta11 * datar_should[:, -1]
+
+    
+    
+    # plt.figure()
+    # for ii in range(data.shape[1]):
+    #     plt.plot(x, datar[:, ii], label=f"{ii}")
+    #     # plt.plot(x, datar_should[:, ii], label=f"{ii}-should")
+    # plt.title("Should be orthogonal")
+    
+    # plt.legend()
+    # plt.show()
