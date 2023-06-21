178c2aeb475082bad53eed91c1f69db8e9b96b80
Alex Gorodetsky <alex@alexgorodetsky.com>
2023-06-21 15:31:36 -0400

    cleaned up and added documentation to mother wavelet basis

--- c3_cex/examples/wavelets1d/main.c
+++ c3_cex/examples/wavelets1d/main.c
@@ -23,166 +23,98 @@ void print_code_usage (FILE * stream, int exit_code)
     exit (exit_code);
 }
 
-int eval_q(size_t N, size_t order,
-		   const double * xinput_array,
-		   const double * alpha, double * out)
+/************************************************************//**************** 
+	Evaluate piecewise-polynomial basis orthogonal to polynomials up to *order*
+    
+    \param[in] N            - Number of data points
+    \param[in] order        - Polynomial order (order + 1 total basis functions)
+    \param[in] xinput_array - (N, ) locations at which to evaluate
+    \param[in] alpha        - (order+1 \times order+1) coefficients
+    \param[in] out          - (order * N) (N columns), evaluations of the basis
+
+    \returns 0 on success
+
+    \note Basis functions of the form
+          \[ 
+             q_j(x) = \tilde{q}_j(x) + \sum_{k=0}^{order} alpha_{kj} p_j(x) 
+          \]
+          where $p_j = x^j$ for $j = 0,\ldots,order$  and 
+          $\tilde{q}_j(x) = p_j(x)$ for $x \geq 0$ and $\tilde{q}_j(x) = - p_j(x)$ for $x < 0$
+ 
+*******************************************************************************/
+int eval_q(size_t N, size_t order, const double * xinput_array, const double * alpha, double * out)
 {
 	size_t n = order+1;
 	/* is it faster to do two loops, one with an if statement and one without? */
 	for (size_t sample_ind = 0; sample_ind < N; sample_ind++){
 		double x = xinput_array[sample_ind];
-		/* printf("ii = %zu\n", sample_ind); */
-		/* if (x >= 0.5) { */
-		// loop over order
-		double xin = 1.0;
-		for (size_t jj = 0; jj < n; jj++) {
-
-			/* \tilde{q} */
-			/* if (x >= 0.5) { */
-			if (x >= 0.0) {
+		if (x >= 0.0) {
+			double xin = 1.0;
+			for (size_t jj = 0; jj < n; jj++){
 				out[sample_ind * n + jj] = xin;
+				xin *= x;
 			}
-			else{
+		}
+		else{
+			double xin = 1.0;
+			for (size_t jj = 0; jj < n; jj++){
 				out[sample_ind * n + jj] = -xin;
+				xin *= x;
 			}
+		}
+	}
 
+	for (size_t sample_ind = 0; sample_ind < N; sample_ind++){
+		double x = xinput_array[sample_ind];
+		for (size_t jj = 0; jj < n; jj++) {
 			double xout = 1.0;
 			for (size_t ii = 0; ii < n; ii++) {
 				out[sample_ind * n + jj] += alpha[jj * n + ii]  * xout;
 				xout = xout * x;
 			}
-			xin = xin * x;
 		}
-		/* } */
-		/* else { */
-		/* 	double xin = 1.0; */
-		/* 	for (size_t jj = 0; jj < n; jj++) { */
-
-		/* 		/\* \tilde{q} *\/ */
-		/* 		out[sample_ind * n + jj] = -xin; */
-
-		/* 		double xout = 1.0; */
-		/* 		for (size_t ii = 0; ii < n; ii++) { */
-		/* 			out[sample_ind * n + jj] += alpha[jj * n + ii]  * xout; */
-		/* 			xout = xout * x; */
-		/* 		} */
-		/* 		xin = xin * x; */
-		/* 	} */
-		/* } */
 	}
 
 	return 0;
 }
 
-int eval_r(size_t N, size_t order,
-		   const double * beta,
-		   double * qvals)
+/************************************************************//**************** 
+	Evaluate piecewise-polynomial orthonormal basis
+    
+    \param[in]     N      - Number of data points
+    \param[in]     order  - Polynomial order (order + 1 total basis functions)
+    \param[in]     beta   - (order+1 \times order+1) coefficients
+    \param[in,out] qvals  - (order * N) (N columns), 
+                            On entry, evaluations of base (non-orthogonal) basis
+                            On exit, evaluation of the basis
+
+    \returns 0 on success
+
+    \note Basis functions of the form, qvals come from eval_q
+          \[ 
+             r_j(x) = q_j(x) + \sum_{k=j+1}^{order} beta_{kj} r_j(x) 
+          \]
+          
+ 
+*******************************************************************************/
+int eval_r(size_t N, size_t order, const double * beta, double * qvals)
 {
 	int n = (int)order+1;
 	/* is it faster to do two loops, one with an if statement and one without? */
 	for (size_t sample_ind = 0; sample_ind < N; sample_ind++){
 		for (int jj = order; jj >= 0; jj--) {
-			/* qvals[sample_ind * n + jj] /= beta[order * n + jj];  */
 			for (size_t kk = jj+1; kk <= order; kk++) {
 				qvals[sample_ind * n + jj] += beta[jj * n + kk] * qvals[sample_ind * n + kk];
 			}
 
 		}
-		/* /\* normalize *\/ */
+		/* normalize */
 		for (int jj = order; jj >= 0; jj--) {
 			qvals[sample_ind * n + jj] /= sqrt(beta[order * n + jj]);
 		}
 	}
 
 	return 0;
-}
-
-void test_q_orthogonality(double lb, double ub, size_t N, size_t order, double * coeff, double *Q)
-{
-	
-	double *x = linspace(lb, ub, N);
-	double *evals = malloc(N * (order+1) * sizeof(double));
-	eval_q(N, order, x, coeff, evals);
-
-	double * orth_mat = calloc((order+1) * (order+1), sizeof(double));
-	for (size_t ii = 0; ii < N-1; ii++){
-		for (size_t jj = 0; jj <= order; jj++) {
-			for (size_t kk = 0; kk <= order;  kk++) {
-				orth_mat[jj * (order+1) + kk] +=
-					0.5 * (evals[ii * (order+1) + jj] * evals[ii * (order+1) + kk] +
-						   evals[(ii+1) * (order+1) + jj] * evals[(ii + 1) * (order+1) + kk]) *
-					(x[ii+1] - x[ii]);
-			}
-		}
-	}
-
-	printf("Numerical Q\n");
-	for (size_t ii = 0; ii <= order; ii++) {
-		for (size_t jj = 0; jj <= order; jj++) {
-			printf("%3.2E ", orth_mat[jj * (order+1) + ii]);
-		}
-		printf("\n");
-	}
-
-	printf("Analytical Q\n");	
-	for (size_t ii = 0; ii <= order; ii++) {
-		for (size_t jj = 0; jj <= order; jj++) {
-			printf("%3.2E ", Q[jj * (order+1) + ii]);
-		}
-		printf("\n");
-	}	
-
-	printf("\n\n\n");
-	free(x); x = NULL;
-	free(evals); evals = NULL;
-	free(orth_mat); orth_mat = NULL;
-}
-
-void test_r_orthogonality(double lb, double ub, size_t N, size_t order, double * coeffA, double * coeffB, double *Q)
-{
-	
-	double *x = linspace(lb, ub, N);
-	double *evals = malloc(N * (order+1) * sizeof(double));
-	eval_q(N, order, x, coeffA, evals);
-	eval_r(N, order, coeffB, evals);
-
-	double * orth_mat = calloc((order+1) * (order+1), sizeof(double));
-	for (size_t ii = 0; ii < N-1; ii++){
-		for (size_t jj = 0; jj <= order; jj++) {
-			for (size_t kk = 0; kk <= order;  kk++) {
-				orth_mat[jj * (order+1) + kk] +=
-					0.5 * (evals[ii * (order+1) + jj] * evals[ii * (order+1) + kk] +
-						   evals[(ii+1) * (order+1) + jj] * evals[(ii + 1) * (order+1) + kk]) *
-					(x[ii+1] - x[ii]);
-			}
-		}
-	}
-
-	printf("Numerical R\n");
-	for (size_t ii = 0; ii <= order; ii++) {
-		for (size_t jj = 0; jj <= order; jj++) {
-			printf("%3.2E ", orth_mat[jj * (order+1) + ii]);
-		}
-		printf("\n");
-	}
-
-	/* now check if beta conditions satisfied */
-	printf("Coefficient errors\n");
-	for (size_t ii = 0; ii <= order; ii++) {
-		for (size_t jj = 0; jj < ii; jj++) {
-			double val = - Q[ii * (order+1) + jj];
-			for (size_t kk = ii+1; kk <= order; kk++) {
-				val += coeffB[ii * (order+1) + kk] * coeffB[jj * (order+1) + kk] * coeffB[order * (order+1) + kk];
-			}
-			val /= coeffB[order * (order+1) + ii];
-			printf("error [%zu, %zu] = %3.2E\n", ii, jj, val - coeffB[ jj * (order + 1) + ii]);
-		}
-	}
-
-	printf("\n\n\n");
-	free(x); x = NULL;
-	free(evals); evals = NULL;
-	free(orth_mat); orth_mat = NULL;
 }
 
 /************************************************************//**************** 
@@ -296,46 +228,175 @@ int orthogonalize(size_t order, const double *Q, double * beta)
 }
 
 
+void test_q_orthogonality(double lb, double ub, size_t N, size_t order, double * coeff, double *Q)
+{
+	
+	double *x = linspace(lb, ub, N);
+	double *evals = malloc(N * (order+1) * sizeof(double));
+	eval_q(N, order, x, coeff, evals);
+
+	double * orth_mat = calloc((order+1) * (order+1), sizeof(double));
+	for (size_t ii = 0; ii < N-1; ii++){
+		for (size_t jj = 0; jj <= order; jj++) {
+			for (size_t kk = 0; kk <= order;  kk++) {
+				orth_mat[jj * (order+1) + kk] +=
+					0.5 * (evals[ii * (order+1) + jj] * evals[ii * (order+1) + kk] +
+						   evals[(ii+1) * (order+1) + jj] * evals[(ii + 1) * (order+1) + kk]) *
+					(x[ii+1] - x[ii]);
+			}
+		}
+	}
+
+	printf("Numerical Q\n");
+	for (size_t ii = 0; ii <= order; ii++) {
+		for (size_t jj = 0; jj <= order; jj++) {
+			printf("%3.2E ", orth_mat[jj * (order+1) + ii]);
+		}
+		printf("\n");
+	}
+
+	printf("Analytical Q\n");	
+	for (size_t ii = 0; ii <= order; ii++) {
+		for (size_t jj = 0; jj <= order; jj++) {
+			printf("%3.2E ", Q[jj * (order+1) + ii]);
+			double error = Q[jj * (order+1) + ii] - orth_mat[jj * (order+1) + ii];
+			assert (fabs(error) < 1e-5);
+		}
+		printf("\n");
+	}	
+
+	printf("\n\n\n");
+	free(x); x = NULL;
+	free(evals); evals = NULL;
+	free(orth_mat); orth_mat = NULL;
+}
+
+void test_r_orthogonality(double lb, double ub, size_t N, size_t order, double * coeffA, double * coeffB, double *Q)
+{
+	
+	double *x = linspace(lb, ub, N);
+	double *evals = malloc(N * (order+1) * sizeof(double));
+	eval_q(N, order, x, coeffA, evals);
+	eval_r(N, order, coeffB, evals);
+
+	double * orth_mat = calloc((order+1) * (order+1), sizeof(double));
+	for (size_t ii = 0; ii < N-1; ii++){
+		for (size_t jj = 0; jj <= order; jj++) {
+			for (size_t kk = 0; kk <= order;  kk++) {
+				orth_mat[jj * (order+1) + kk] +=
+					0.5 * (evals[ii * (order+1) + jj] * evals[ii * (order+1) + kk] +
+						   evals[(ii+1) * (order+1) + jj] * evals[(ii + 1) * (order+1) + kk]) *
+					(x[ii+1] - x[ii]);
+			}
+		}
+	}
+
+	printf("Numerical R\n");
+	for (size_t ii = 0; ii <= order; ii++) {
+		for (size_t jj = 0; jj <= order; jj++) {
+			printf("%3.2E ", orth_mat[jj * (order+1) + ii]);
+		}
+		printf("\n");
+	}
+
+	/* now check if beta conditions satisfied */
+	printf("Coefficient errors\n");
+	for (size_t ii = 0; ii <= order; ii++) {
+		for (size_t jj = 0; jj < ii; jj++) {
+			double val = - Q[ii * (order+1) + jj];
+			for (size_t kk = ii+1; kk <= order; kk++) {
+				val += coeffB[ii * (order+1) + kk] * coeffB[jj * (order+1) + kk] * coeffB[order * (order+1) + kk];
+			}
+			val /= coeffB[order * (order+1) + ii];
+			double error = val - coeffB[ jj * (order + 1) + ii];
+			printf("error [%zu, %zu] = %3.2E\n", ii, jj, val - coeffB[ jj * (order + 1) + ii]);
+			assert (fabs(error) <= 1e-12);
+		}
+	}
+
+	/* printf("\n\n\n"); */
+	free(x); x = NULL;
+	free(evals); evals = NULL;
+	free(orth_mat); orth_mat = NULL;
+}
+
+
+/** \struct MotherBasis 
+ * \brief Contains coefficients for evaluating the mother basis multiresolution function
+ * \var MotherBasis::order 
+ * polynomial order to which the basis is orthogonal to 
+ * \var MotherBasis::acoeff
+ * coefficients for the evaluation of an intermediate basis orthogonal to global polynomials up to stated order
+ * \var MotherBasis::bcoeff 
+ * coefficients for the orthogonalization of the basis defined by acoeff 
+ * \var MotherBasis::Q 
+ * inner product between intermediate (non-orthogonal) basis functions
+ */
 struct MotherBasis
 {
 	size_t order;
 	double * acoeff;
 	double * bcoeff;
+	double * Q;
 };
 
+/***********************************************************//**
+    Allocate space for a MotherBsais
+
+    \param[in] order - polynomial order
+
+    \return mb - MotherBasis
+***************************************************************/
 struct MotherBasis * mother_basis_alloc(size_t order)
 {
 	struct MotherBasis * mb = malloc(sizeof(struct MotherBasis));
 	mb->order = order;
 	mb->acoeff = calloc((order+1) * (order+1), sizeof(double));
 	mb->bcoeff = calloc((order+1) * (order+1), sizeof(double));
-
+	mb->Q = calloc((order+1) * (order+1), sizeof(double));
 	return mb;
 }
 
+/***********************************************************//**
+    Free Memory of a MotherBasis
+
+    \param[in,out] mb - struct to free
+***************************************************************/
 void mother_basis_free(struct MotherBasis * mb)
 {
 	if (mb != NULL) {
 		free(mb->acoeff); mb->acoeff = NULL;
 		free(mb->bcoeff); mb->bcoeff = NULL;
+		free(mb->Q); mb->Q = NULL;
 		free(mb); mb = NULL;
 	}
 }
 
+/***********************************************************//**
+    Compute coefficients defining the MotherBasis
+
+    \param[in,out] mb - MotherBasis
+
+	\returns 0 on success
+***************************************************************/
 int mother_basis_compute_coeffs(struct MotherBasis * mb)
 {
-	size_t n2 = (mb->order+1) * (mb->order+1);
-	double * Q = calloc(n2, sizeof(double));
 
-	int orth_res = orthogonalize_wrt_to_poly(mb->order, mb->acoeff, Q);
+	int orth_res = orthogonalize_wrt_to_poly(mb->order, mb->acoeff, mb->Q);
 	assert(orth_res == 0);
-	int beta_res = orthogonalize(mb->order, Q, mb->bcoeff);
+	int beta_res = orthogonalize(mb->order, mb->Q, mb->bcoeff);
 	assert(beta_res == 0);
 
-	free(Q); Q = NULL;
 	return 0;
 }
 
+/***********************************************************//**
+    Create (alloc and compute coefficients) for a mother basis
+
+    \param[in] order - polynomial order
+
+	\returns mb - MotherBasis
+***************************************************************/
 struct MotherBasis * mother_basis_create(size_t order)
 {
 	struct MotherBasis * mb = mother_basis_alloc(order);
@@ -343,6 +404,11 @@ struct MotherBasis * mother_basis_create(size_t order)
 	return mb;
 }
 
+/***********************************************************//**
+    Evaluate the non-orthogonal portion of the basis 
+
+	\see @eval_q
+***************************************************************/
 int mother_basis_eval_non_orth(const struct MotherBasis * mb, size_t N, const double * x, double * eval)
 {
 	assert (mb != NULL);
@@ -350,6 +416,11 @@ int mother_basis_eval_non_orth(const struct MotherBasis * mb, size_t N, const do
 	return res;
 }
 
+/***********************************************************//**
+    Evaluate the basis
+
+	\see @eval_q and @eval_r
+***************************************************************/
 int mother_basis_eval(const struct MotherBasis * mb, size_t N, const double * x, double * eval)
 {
 	assert (mb != NULL);
@@ -454,6 +525,9 @@ int main(int argc, char * argv[])
 	
 	struct MotherBasis * mb = mother_basis_create(order);
 
+	test_r_orthogonality(-1.0, 1.0, 1000000, order, mb->acoeff, mb->bcoeff,  mb->Q);
+	test_q_orthogonality(-1.0, 1.0, 1000000, order, mb->acoeff, mb->Q);
+		
 	double lb = -1.0;	
 	double ub = 1.0;
 	size_t N = 100000;
